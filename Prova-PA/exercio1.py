7cc7f7d7402280580a566656d1097eb3f6b6a9a1
luisguilhermemcm-creator <luisguilhermemcm@gmail.com>
2026-06-23 14:16:16 -0300

    Python vs code

---
+++ Prova-PA/exercio1.py
@@ -0,0 +1,13 @@
+# QUESTÃO 01 – Contagem Crescente (2 pontos)
+# Elabore um programa que exiba na tela os números de 1 até 20,
+# um em cada linha.
+# Exemplo de saída:
+# 1
+# 2
+# 3
+# ...
+# 20
+
+for i in range (1, 21):
+    print (i)
+
